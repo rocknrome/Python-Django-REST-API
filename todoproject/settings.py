2086ecb4a2c0984a5ddf4a2cd04bf98b667899e4
Roman Larionov <pcsaveddotcom@gmail.com>
2024-03-23 11:54:32 -0400

    class work-before break-2

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todos.apps.TodosConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
